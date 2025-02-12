382c191ee60783280a053bf283e5acf998f74d0c
madhusudan1das <148602555+madhusudan1das@users.noreply.github.com>
2025-02-12 08:33:14 +0530

    java script

---
+++ script.js
@@ -0,0 +1,69 @@
+// console.log('hello world');
+// let a = 18;
+// let b="hello";             //string
+// console.log( a);          //print the output
+// console.log(typeof b);   //print type of data
+ 
+// const student_Id={
+//  name:"madhusudan das",
+//  age:21,                                //odject
+//  class:"2nd year",
+//  marks:8.2,
+//  isPass:true,
+// };
+
+// student_Id["age"]=student_Id["age"]+2;  
+// student_Id["name"]="sanu";                  // upadte any odject data useing  it      
+// console.log(student_Id)
+
+
+// function exampleVar() {
+//     console.log(x); // Output: undefined (hoisting)
+//     var x = 10;
+//     console.log(x); // Output: 10
+//     var x = 20;                                       //var example
+//     console.log(x); // Output: 20
+//   }
+// exampleVar();
+
+
+
+// function exampleLet() {
+//     // console.log(y);  // Error: Cannot access 'y' before initialization (TDZ)
+//     let y = 10;
+//     console.log(y); // Output: 10                                                       //let example
+//     y = 20;
+//     console.log(y); // Output: 20
+//     // let y = 30;    // Error: Identifier 'y' has already been declared
+//   }
+//   exampleLet();
+
+
+
+// function exampleConst() {
+//     const z = 10;
+//     console.log(z); // Output: 10
+//     // z = 20;        // Error: Assignment to constant variable                      //const example
+//     // const z = 30;  // Error: Identifier 'z' has already been declared
+    
+//     const obj = {name: "John"};
+//     obj.name = "Jane"; // Allowed, as we are modifying the object, not the variable itself
+//     console.log(obj.name) // Output: Jane
+//   }
+//   exampleConst();
+
+
+// // operators 
+// window.click = function() {
+//     window.location.href = "https://www.w3schools.com/js/js_operators.asp";
+// };
+
+// let a=2;
+// let b=2;
+// b=3;
+// a=3;
+// console.log(a<=b);            //comparison
+
+
+
+
